8d4bf7722e1b284c4aaedf637740f3c720f7172e
Amaldev <88559916+amaldev07@users.noreply.github.com>
2023-09-02 00:59:36 +0530

    Create generatorCode.ts

---
+++ generatorCode.ts
@@ -0,0 +1,29 @@
+# Code
+```
+/**
+ * Encodes a URL to a shortened URL.
+ */
+let baseUrl = "https://tinyUrl.com/"
+let hm = new Map();
+function encode(longUrl: string): string {
+    let encyUrl = String.fromCharCode(Math.random() * 100);
+    while (hm.get(encyUrl) != null) {
+        encyUrl = encyUrl + String.fromCharCode(Math.random() * 100);
+    }
+    hm.set(encyUrl, longUrl)
+    return baseUrl + encyUrl;
+};
+
+/**
+ * Decodes a shortened URL to its original URL.
+ */
+function decode(shortUrl: string): string {
+    let origin = (new URL(shortUrl)).origin;
+    return hm.get(shortUrl.split(baseUrl)[1]);
+};
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(strs));
+ */
+```
